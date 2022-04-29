de063c96450c51fc36b40513b1408bfa32bcf32b
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-28 21:26:53 -0400

    Add body-parser and nodemon as dependencies

--- express_server.js
+++ express_server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require("body-parser");
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
