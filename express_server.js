f3808381f98624c2a6712a5341e9c0b10f83ff5b
Yoyo Yang <yoyo.truly@gmail.com>
2022-05-01 10:52:53 -0400

    Re-organize require statements

--- express_server.js
+++ express_server.js
@@ -1,8 +1,8 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const PORT = 8080;
-
-const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
