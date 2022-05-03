58e63d35eb1e0a1a7958767de7aca055a6f0de4c
Yoyo Yang <yoyo.truly@gmail.com>
2022-05-03 18:32:21 -0400

    Separate invalid registration condition and user exists condition

--- routes/register.js
+++ routes/register.js
@@ -17,16 +17,15 @@ router.post("/", (req, res) => {
   const { email, password } = req.body;
   const id = generateRandomString();
 
-  if (!email || !password || checkEmailExist) {
-    return res.status(400).send("Registration information invalid");
-  }
+  if (!email || !password) return res.status(400).send("Registration information invalid");
+  if (checkEmailExist) return res.status(400).send("User already exists.");
 
   users[id] = {
     id,
     email,
     password
   };
-
+  
   res
     .cookie("id", id)
     .redirect("/urls");
