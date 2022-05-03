ca5e487847878470bfe96136ce718386b4e419fb
Yoyo Yang <yoyo.truly@gmail.com>
2022-05-03 18:43:08 -0400

    Add a standalone login page

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,16 @@
 const express = require("express");
 
+const { users } = require("../constants");
+
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  const templateVars = {
+    user: users[req.cookies.id]
+  };
+
+  res.render("login", templateVars);
+});
 
 // TODO: need to change login to take email and password instead
 router.post("/", (req, res) => {
