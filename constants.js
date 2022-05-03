170b3e94c2286d1854d3121c181d0bf5cd2d700b
Yoyo Yang <yoyo.truly@gmail.com>
2022-05-03 17:22:26 -0400

    Refactor temporary constants to its own module

---
+++ constants.js
@@ -0,0 +1,22 @@
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+const users = {
+  u1: {
+    id: "u1",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  u2: {
+    id: "u2",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+module.exports = {
+  urlDatabase,
+  users
+};
