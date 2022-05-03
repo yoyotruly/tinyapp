6bda87d3338cb1a341b1c0b30e67f7fa309e2eaa
Yoyo Yang <yoyo.truly@gmail.com>
2022-05-03 18:17:42 -0400

    Add helper function to check if an email exists during user registration

--- utils.js
+++ utils.js
@@ -1,7 +1,17 @@
+const { users } = require("./constants");
+
 const generateRandomString = () => {
   return "x2Az3";
 };
 
+const checkEmailExist = (email) => {
+  const emailRecords = Object.values(users).map(x => x.email);
+
+  return emailRecords.includes(email);
+};
+
+
 module.exports = {
-  generateRandomString
+  generateRandomString,
+  checkEmailExist
 };
