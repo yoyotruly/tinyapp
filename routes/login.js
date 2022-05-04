652bf8925af30d340b8995ce47329db9b1182d9b
Yoyo Yang <yoyo.truly@gmail.com>
2022-05-04 17:32:28 -0400

    Add new login validation logic with email and password instead of username

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,23 @@
 const express = require("express");
 
+const { findUser } = require("../utils/utils");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("login");
-});
+router
+  .route("/")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post((req, res) => {
+    const { email, password } = req.body;
+    const user = findUser(email, password);
+    
+    if (!user) return res.status(403).send("Oops, user doesn't exist");
+    
+    res
+      .cookie("user_id", user.id)
+      .redirect("/urls");
+  });
 
 module.exports = router;
--- utils/utils.js
+++ utils/utils.js
@@ -10,7 +10,27 @@ const checkEmailExist = (email) => {
   return emailRecords.includes(email);
 };
 
+/**
+ * Check if a user is valid with provided email and password, return the user
+ * object if it is valid.
+ * @param {string} email Email
+ * @param {string} password Password
+ * @returns {*} Returns user object if input email and password match user's
+ * record, returns false otherwise
+ */
+const findUser = (email, password) => {
+  if (!email || !password) return false;
+  
+  const user = Object.values(users)
+    .find((user) => (user.email === email && user.password === password));
+
+  if (!user) return false;
+
+  return user;
+};
+
 module.exports = {
   generateRandomString,
-  checkEmailExist
+  checkEmailExist,
+  findUser
 };
