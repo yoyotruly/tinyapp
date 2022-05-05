62803c45256ece97a6798e1e19e08cb1bb5e9f1c
Yoyo Yang <yoyo.truly@gmail.com>
2022-05-04 21:25:48 -0400

    Expand urlDatabase to include user id as well

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,24 @@
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userId: "u1"
+  },
+  "a1Ea5x": {
+    longURL: "",
+    userId: "u1"
+  },
+  "ty5W3Q": {
+    longURL: "",
+    userId: "u1"
+  },
+  "9aiU7q": {
+    longURL: "",
+    userId: "u2"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userId: "u2"
+  },
 };
 
 const users = {
