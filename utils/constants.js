bdb559f39ef030fd0387e942b237f6e88c01a029
Yoyo Yang <yoyo.truly@gmail.com>
2022-05-06 22:55:15 -0400

    Add analytics to urlDatabase property

--- utils/constants.js
+++ utils/constants.js
@@ -1,23 +1,43 @@
 const urlDatabase = {
   "b2xVn2": {
     longURL: "https://www.lighthouselabs.ca",
-    userId: "u1"
+    userId: "u1",
+    analytics: {
+      totalClicks: 0,
+      uniqueVistors: 0
+    }
   },
   "a1Ea5x": {
     longURL: "https://www.youtube.com/",
-    userId: "u1"
+    userId: "u1",
+    analytics: {
+      totalClicks: 0,
+      uniqueVistors: 0
+    }
   },
   "ty5W3Q": {
     longURL: "https://www.facebook.com",
-    userId: "u1"
+    userId: "u1",
+    analytics: {
+      totalClicks: 0,
+      uniqueVistors: 0
+    }
   },
   "9aiU7q": {
     longURL: "https://www.airbnb.ca/",
-    userId: "u2"
+    userId: "u2",
+    analytics: {
+      totalClicks: 0,
+      uniqueVistors: 0
+    }
   },
   "9sm5xK": {
     longURL: "https://www.google.com",
-    userId: "u2"
+    userId: "u2",
+    analytics: {
+      totalClicks: 0,
+      uniqueVistors: 0
+    }
   },
 };
 
