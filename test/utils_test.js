02c751dca0762e8de6b4a6b270b2c6c5a1bf29ed
Yoyo Yang <yoyo.truly@gmail.com>
2022-05-07 10:42:03 -0400

    Add test for getUserByLogin

--- test/utils_test.js
+++ test/utils_test.js
@@ -1,15 +1,16 @@
+const { expect } = require("chai");
 const utils = require("../utils/utils");
 
 const testUsers = {
   "userRandomID": {
     id: "userRandomID",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur"
+    password: "$2a$10$fVkM8H6Pg93GuctBKz/GgOduNV86yB2WVXpZgD9fsdiu20amQmOkq" // hashed from purple-monkey-dinosaur
   },
   "user2RandomID": {
     id: "user2RandomID",
     email: "user2@example.com",
-    password: "dishwasher-funk"
+    password: "$2a$10$Q5vF64y83RTKBjIwMuiykexzOzHyP4QaHQ3ZnCwqYPwzH78Z19xnC" // hashed from dishwasher-funk
   }
 };
 
@@ -22,11 +23,57 @@ describe("isExistingEmail(email, userDb)", () => {
 });
 
 describe("isUserAuthorized(userId, value, lookupBy, userDb)", () => {
-  // utils.isUserAuthorized();
+  // utils.isUserAuthorized;
 });
 
 describe("getUserByLogin(email, password, userDb)", () => {
-  // utils.getUserByLogin();
+  it("should return a user object with valid email and password", () => {
+    const user = utils.getUserByLogin(
+      "user@example.com",
+      "purple-monkey-dinosaur",
+      testUsers
+    );
+    const expectedOutput = testUsers.userRandomID;
+
+    expect(user).to.eql(expectedOutput);
+  });
+
+  it("should return undefined with an invalid email", () => {
+    const user = utils.getUserByLogin(
+      "fakeemail@example.com",
+      "purple-monkey-dinosaur",
+      testUsers
+    );
+
+    expect(user).to.equal(undefined);
+  });
+
+  it("should return undefined with an invalid password", () => {
+    const user = utils.getUserByLogin(
+      "user@example.com",
+      "purple-monkey",
+      testUsers
+    );
+
+    expect(user).to.equal(undefined);
+  });
+
+  it("should return undefined with an empty email or password", () => {
+    const user1 = utils.getUserByLogin(
+      "",
+      "purple-monkey-dinosaur",
+      testUsers
+    );
+
+    const user2 = utils.getUserByLogin(
+      "user@example.com",
+      "",
+      testUsers
+    );
+
+    expect(user1).to.equal(undefined);
+    expect(user2).to.equal(undefined);
+  });
 });
 
 describe("getUserById(id, userDb)", () => {
